0d664a4f91c1c435b9129a9addc02a5bd7a7dd82
TerrificMao <maodanrong_buaa@163.com>
2017-03-26 02:13:57 +0800

    add lambda and yaml code

---
+++ index.js
@@ -0,0 +1,9 @@
+var time = require('time');
+exports.handler = (event, context, callback) => {
+    var currentTime = new time.Date(); 
+    currentTime.setTimezone("Asia/Shanghai");
+    callback(null, {
+        statusCode: '200',
+        body: 'The time in China Beijing is: ' + currentTime.toString(),
+    });
+};
