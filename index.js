61ec26660d038a04047198048ae3703564c276d9
TerrificMao <maodanrong_buaa@163.com>
2017-04-16 22:22:08 +0800

    Change the body description

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var time = require('time');
 exports.handler = (event, context, callback) => {
-    var currentTime = new time.Date(); 
+    var currentTime = new time.Date();
     currentTime.setTimezone("Asia/Shanghai");
     callback(null, {
         statusCode: '200',
-        body: 'The Beijing time is: ' + currentTime.toString(),
+        body: 'Hello, The China Beijing time is: ' + currentTime.toString(),
     });
 };
