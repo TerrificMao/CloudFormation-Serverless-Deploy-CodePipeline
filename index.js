25efedc7c4c2603e8ae891a161f37ae7945ddce8
TerrificMao <maodanrong_buaa@163.com>
2017-11-03 10:21:46 +0800

    change !

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ exports.handler = (event, context, callback) => {
     currentTime.setTimezone("Asia/Shanghai");
     callback(null, {
         statusCode: '200',
-        body: 'Hello, the beijing time is: ' + currentTime.toString(),
+        body: 'Hello！ the beijing time is: ' + currentTime.toString(),
     });
 };
